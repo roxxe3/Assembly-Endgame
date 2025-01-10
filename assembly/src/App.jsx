f27b67f876ee8f0b2912fb07050c3252b905938f
roxxe3 <hamzafarissi@gmail.com>
2025-01-10 19:53:20 +0100

    Refactor letter selection and display logic; improve visual feedback for chosen letters

--- assembly/src/App.jsx
+++ assembly/src/App.jsx
@@ -1,70 +1,97 @@
-import Word from "./Word"
-import Letters from "./Letter"
-import {words , lettersArray} from "./words.js"
-import { useState } from "react"
+import Word from "./Word";
+import Letters from "./Letter";
+import { words, lettersArray } from "./words.js";
+import { useState } from "react";
 
-function chooseRandWord(){
-  return words[Math.floor(Math.random() * words.length)]
+function chooseRandWord() {
+  return words[Math.floor(Math.random() * words.length)];
 }
 
-export default function App(){
-  const [word, setWord] = useState(chooseRandWord())
-  const [letterObj, setLetterObj] = useState(genratLetterObject())
+export default function App() {
+  const [word, setWord] = useState(chooseRandWord());
+  const [letterObj, setLetterObj] = useState(genratLetterObject());
+  const [displayedLetters, setDisplayedLetters] = useState(Array(word.length).fill(''));
 
-  function genratLetterObject(){
+  function getCharIndex(letter) {
+    const indexes = [];
+    for (let i = 0; i < word.length; i++) {
+      if (word[i].toLowerCase() === letter.toLowerCase()) {
+        indexes.push(i);
+      }
+    }
+    console.log(`Indexes for letter "${letter}":`, indexes);
+    return indexes;
+  }
+
+  console.log(word);
+
+  function genratLetterObject() {
     return lettersArray.map(letter => ({
       value: letter,
       isChosen: false,
       isFalse: false
-    }))
+    }));
   }
 
-  const LettersEl = letterObj.map(letter => 
-                <Letters 
-                        key={letter.value} 
-                        value={letter.value}
-                        isChosen ={letter.isChosen}
-                        isFalse = {letter.isFalse}
-                        chose={()=> checkLeter(letter.value)}
-                  />)
+  const LettersEl = letterObj.map(letter => (
+    <Letters 
+      key={letter.value} 
+      value={letter.value}
+      isChosen={letter.isChosen}
+      isFalse={letter.isFalse}
+      chose={() => checkLeter(letter.value)}
+    />
+  ));
 
-function checkLeter(letter) {
+  function updateDisplayedLetters(letter) {
+    const indexes = getCharIndex(letter);
+    console.log(indexes);
+    indexes.forEach(index => {
+      setDisplayedLetters(prevDisplayed => {
+        const newDisplayed = [...prevDisplayed];
+        newDisplayed[index] = letter;
+        return newDisplayed;
+      });
+    });
+  }
+
+  function checkLeter(letter) {
     setLetterObj(prevLetterObj => {
-        const updatedLetterObj = prevLetterObj.map(obj => {
-            if (obj.value === letter) {
-                if (word.includes(letter.toLowerCase())) {
-                    return {
-                              ...obj,
-                              isChosen: true
-                            };
-                          } else {
-                            return {
-                              ...obj,
-                              isFalse: true
-                            };
-                          }
-                        } else {
-                          return obj;
-                        }
-                      });
-                      return updatedLetterObj;
-                    });
-}
+      const updatedLetterObj = prevLetterObj.map(obj => {
+        if (obj.value === letter) {
+          if (word.includes(letter.toLowerCase())) {
+            updateDisplayedLetters(letter)
+            return {
+              ...obj,
+              isChosen: true
+            };
+          } else {
+            return {
+              ...obj,
+              isFalse: true
+            };
+          }
+        } else {
+          return obj;
+        }
+      });
+      console.log(displayedLetters);
+      return updatedLetterObj;
+    });
+  }
 
-const spanEl = []
-for(let i= 0 ; i < word.length; i++){
-    spanEl.push(<Word key={i} letter={word[i].toUpperCase()}  />)
-}
-
+  const spanEl = displayedLetters.map((letter, index) => (
+    <Word key={index} letter={letter} />
+  ));
 
   return (
     <>
-    <section>
-      {spanEl}
-    </section>
+      <section>
+        {spanEl}
+      </section>
       <div className="letters">
         {LettersEl}
       </div>
-      </>
-  )
+    </>
+  );
 }
